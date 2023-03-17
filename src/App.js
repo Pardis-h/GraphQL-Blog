45befd1c64d4004c0f563775ec301b964e564c9f
Pardis-H <pardis.haghdoust@gmail.com>
2023-03-17 11:34:39 +0330

    Create Layout Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Header from './components/layout/Header';
+import HomePage from './components/home/HomePage';
+import Layout from './components/layout';
+
 
 function App() {
   return (
     <>
-      <Header/>
+      <Layout>
+        <HomePage/>
+      </Layout>
     </>
   );
 }
