1c43252642a086f172a4581377e03d531137eed6
Pardis-H <pardis.haghdoust@gmail.com>
2023-03-17 21:51:36 +0330

    Get Author Data using params slug

--- src/components/authors/AuthorPage.js
+++ src/components/authors/AuthorPage.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useQuery } from "@apollo/client";
+import { GET_AUTHOR_INFO } from '../../graphql/queries';
+import { useParams } from 'react-router-dom';
 
 function AuthorPage() {
+
+    const {slug} = useParams();
+    // console.log(slug);
+
+    const {loading, data, errors} = useQuery(GET_AUTHOR_INFO, {variables:{slug}});
+    console.log(data);
+
+    if(loading) return <h1>Loading...</h1>
+
+    if(errors) return <h1>Error...</h1>
+
     return (
         <div>
             AuthorsPage
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,38 +1,60 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 const GET_BLOGS_INFO = gql`
-    query {
-        posts {
-            title
-            slug
-            id
-            author {
-            ... on Author {
-                avatar {
-                url
-                }
-                name
-            }
-            }
-            coverPhoto {
+  query {
+    posts {
+      title
+      slug
+      id
+      author {
+        ... on Author {
+          avatar {
             url
-            }
+          }
+          name
         }
+      }
+      coverPhoto {
+        url
+      }
     }
+  }
 `;
 
 const GET_AUTHORS_INFO = gql`
-    query {
-        authors {
-            field
-            id
-            name
-            slug
-            avatar {
-            url
-            }
-        }
+  query {
+    authors {
+      id
+      name
+      slug
+      avatar {
+        url
+      }
     }
+  }
 `;
 
-export {GET_BLOGS_INFO, GET_AUTHORS_INFO};
+const GET_AUTHOR_INFO = gql`
+    query getAuthorInfo($slug: String!) {
+        author(where: {slug: $slug}) {
+            name
+            field
+            description {
+                html
+            }
+            avatar {
+                url
+            }
+            posts {
+                title
+                id
+                slug
+                coverPhoto {
+                    url
+                }
+            }
+        }
+}
+`;
+
+export { GET_BLOGS_INFO, GET_AUTHORS_INFO, GET_AUTHOR_INFO };
